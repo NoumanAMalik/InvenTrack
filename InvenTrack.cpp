dccb3aef445bae6ecb2b0d55cd6e5bbb28a8014a
Nouman <noumancodes@gmail.com>
2022-12-23 18:58:21 -0500

    Open SQL database

--- InvenTrack.cpp
+++ InvenTrack.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sqlite3.h>
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    sqlite3* db;
+    int rc = sqlite3_open("Inventory.db", &db);
+
+    if (rc != SQLITE_OK) {
+        std::cerr << "Error opening SQLite Database: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return EXIT_FAILURE;
+    }
+
+    sqlite3_close(db);
     return EXIT_SUCCESS;
 }

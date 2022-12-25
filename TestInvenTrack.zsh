a18865896222536d655db4407bedcbaa358d67f7
Nouman <noumancodes@gmail.com>
2022-12-25 00:00:50 -0500

    Created script to run test

---
+++ TestInvenTrack.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# A script to test InvenTack.cpp
+
+## First reset the database
+./ResetDatabase.zsh
+
+## Compile InvenTrack.cpp
+g++ -std=c++20 -Wall -Wextra -Werror -l sqlite3 InvenTrack.cpp -o InvenTrack.exe
+
+## Run Compiled .exe
+./InvenTrack.exe -m scanin -upc 1110

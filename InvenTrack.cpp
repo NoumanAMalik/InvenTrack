7654539e6aad490a44b1ee923f900baf3de18f52
Nouman <noumancodes@gmail.com>
2023-03-08 09:31:23 -0500

    Matches input with available commands

--- InvenTrack.cpp
+++ InvenTrack.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main(int argc, char* argv[]) {
-    for (int i = 0; i < argc; i ++) {
-        std::cout << argv[i] << " ";
+    std::vector<std::string> commands {"scanin", "scanout"};
+
+    if (argc < 2) { // There is no command
+        std::cerr << "Error: You forgot to put the mode" << '\n';
+        std::cerr << "Usage: InvenTrack <mode> <...>" << '\n';
     }
+
+    auto foundCommand = std::find(commands.begin(), commands.end(), argv[1]);
+    
+    if (*foundCommand != "") std::cout << *foundCommand << '\n';
+    else std::cout << "Not a command" << '\n';
+
     std::cout << "\n";
 }

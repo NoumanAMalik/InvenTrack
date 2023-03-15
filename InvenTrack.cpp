0ecaf805f28dba172cebcd707d95cb5c61a615a5
Nouman <noumancodes@gmail.com>
2023-03-14 21:42:42 -0400

    Comparing input with command list
    When someone runs the program, the available command list is looped through, looking for a match with the input

--- InvenTrack.cpp
+++ InvenTrack.cpp
@@ -10,5 +10,16 @@ int main(int argc, char* argv[]) {
         std::cerr << "Usage: InvenTrack <mode> <...>" << '\n';
     }
 
+    std::string input {argv[1]};
+
+    std::for_each(
+            begin(commands), end(commands),
+            [input](auto const& command) {
+                if (command == input) {
+                    std::cout << "Found" << '\n';
+                } else {
+                    std::cout << "Not Found" << '\n';
+                }
+            });
 
 }
